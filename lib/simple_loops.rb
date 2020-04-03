681ba6f6ae0227fc8c1d26d8fd85ccb6eb70b6b1
Ian Rosen <irosen419@gmail.com>
2020-04-03 23:53:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,7 +15,7 @@ end
 def output_array(array)
   counter = 0
   
-  while counter <= array.length do
+  while counter <=array.length do
     puts array[counter]
     counter += 1
   end
@@ -24,7 +24,7 @@ end
 def return_string_array(array)
   counter = 0
   
-  while counter <= array.length do
+  while counter <=array.length do
     array[counter] = array[counter].to_s
     counter += 1
   end
